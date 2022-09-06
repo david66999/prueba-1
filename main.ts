d5ead2d168f5daf3d2dad3e11048d62bd0beee2f
david66999 <112917968+david66999@users.noreply.github.com>
2022-09-05 21:49:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,259 @@
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+}
+basic.clearScreen()
+basic.pause(2000)
+basic.showString("ARBOL ")
+for (let index = 0; index < 2; index++) {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showString("CORAZON ")
+for (let index = 0; index < 2; index++) {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . . # #
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # # # #
+        . # # # #
+        . . # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . # . .
+        # # # # .
+        # # # # .
+        . # # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # # . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showString("FANTASMA ")
+for (let index = 0; index < 2; index++) {
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . . # #
+        . . . # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . . # # #
+        . . # # #
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . # .
+        . # # # #
+        . # # # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        # # # # #
+        # . # . #
+        `)
+    basic.showLeds(`
+        # # # . .
+        . # . # .
+        # # # # .
+        # # # # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # # # . .
+        # # # . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # # . . .
+        # # . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+}
+basic.forever(function () {
+	
+})
